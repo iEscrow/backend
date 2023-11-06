679b01712038a1b772e876f4db9fb196f5e986fc
Emiliano Alegre <emilianogalegre@gmail.com>
2023-11-06 17:04:22 -0300

    configuracion con variables de entorno

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'Emiliano',
-  database: 'iescrow',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   define: {
     timestamps: false,
   },
