775dd69bdde4addaf1df5b932981d39929935a93
Emiliano Alegre <emilianogalegre@gmail.com>
2024-04-09 16:42:52 -0300

    config db

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
-  password: process.env.DB_AWS_PASS,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   define: {
     timestamps: false,
